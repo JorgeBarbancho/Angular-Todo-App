a866b7bedd7406632d8ac718a3d3b6e3ae1e0c46
Jorge Barbancho <jorge.barbancho@kps.com>
2021-12-13 17:43:28 +0100

    add list buttons icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodoBoardComponent } from './todo-board/todo-board.component';
+import { ListButtonsComponent } from './list-buttons/list-buttons.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoBoardComponent
+    TodoBoardComponent,
+    ListButtonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list-buttons/list-buttons.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { faTh, faTasks, faList } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-list-buttons',
+  templateUrl: './list-buttons.component.html',
+  styleUrls: ['./list-buttons.component.css']
+})
+export class ListButtonsComponent implements OnInit {
+
+  faTh = faTh;
+  faTasks = faTasks;
+  falList = faList;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
