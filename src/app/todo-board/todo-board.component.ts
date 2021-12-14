3756ab4d268ce651107dc0268ebf61fa9c38e6b7
Jorge Barbancho <jorge.barbancho@kps.com>
2021-12-14 17:37:48 +0100

    start behaviour implementation

--- src/app/todo-board/todo-board.component.ts
+++ src/app/todo-board/todo-board.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import {Task} from '../task';
 
 @Component({
   selector: 'app-todo-board',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoBoardComponent implements OnInit {
 
-  constructor() { }
+  faTrash = faTrash;
+  tasks: Task[] = [];
+  idCounter: number = 1;
+  pendingTasksCount: number = 0;
+  pendingTasksText: string = 'tasks';
+  showingAll: boolean = true;
+  showingPending: boolean = false;
+  showingCompleted: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  addTask(e: Event, newTask: string): void {
+    e.preventDefault();
+    this.tasks.push({id: this.idCounter, pending: true, text: newTask});
+    this.updatePendingTasksText();
+    this.idCounter++;
+  }
+
+  toggleTask(task: Task): void {
+    task.pending = !task.pending;
+    this.updatePendingTasksText();
+  }
+
+  removeTask(task: Task): void {
+    this.tasks = this.tasks.filter(item => item.id !== task.id);
+    this.updatePendingTasksText();
+  }
+
+  updatePendingTasksText(): void {
+    this.pendingTasksCount = this.tasks.filter(task => task.pending).length
+    this.pendingTasksText = this.pendingTasksCount === 1 ? 'task' : 'tasks';
+  }
+
+  showAll(): void {
+    this.showingAll = true;
+    this.showingPending = false;
+    this.showingCompleted = false;
+  }
+
+  showCompleted(): void {
+    this.showingAll = false;
+    this.showingPending = false;
+    this.showingCompleted = true;
+  }
+
+  showPending(): void {
+    this.showingAll = false;
+    this.showingPending = true;
+    this.showingCompleted = false;
+  }
 }
